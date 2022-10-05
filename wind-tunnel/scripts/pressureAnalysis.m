5cdc1fcc765f4d2b8b7205d9289db338e33d1e7a
Antonello <antonello.paolino@iit.it>
2022-10-05 12:29:37 +0200

    Update pressure script for post-processing

--- wind-tunnel/scripts/pressureAnalysis.m
+++ wind-tunnel/scripts/pressureAnalysis.m
@@ -2,78 +2,109 @@ close all;
 clear all; 
 clc;
 
-%% Load geometric data
+%% Initialization
 
-coverName = ['chest'];
+% Test and point to be analyzed
+testID = {'TID_0002'};
+testPointID = {'PT0001'};
+coverNames = {'face_front','chest','rt_thigh_front','lt_thigh_front', ...
+              'rt_shin_front','lt_shin_front'};
 
-geom = importGeometry(['./srcPressureAnalysis/',coverName,'.stl']);
-opts = detectImportOptions('./srcPressureAnalysis/chest_sensors.txt');
-pressureSensors = table2struct(readtable(['./srcPressureAnalysis/',coverName,'_sensors.txt'],opts),"ToScalar",true);
-pressureSensorsNames = pressureSensors.Var1;
-x_pressureSensors    = pressureSensors.Var2;
-y_pressureSensors    = pressureSensors.Var3;
-z_pressureSensors    = pressureSensors.Var4;
+globalMinPress = 1e4;
+globalMaxPress = -1e4;
 
-%% Load pressure data
-addpath(genpath('../'));            % Adding the main folder path
-experiment = 'exp_21_03_22';        % Name of the experiment data folder
-folderPath = ['../',experiment,'/data_Matlab/'];
+for j = 1:length(coverNames)
 
-% Name of the test and test points to be loaded
-testID = {'TID_0001'};
-testPointIDs = {'PT0001'};
+    %% Load geometric data
+    coverName = coverNames{j};
+    geom = importGeometry(['./srcPressureAnalysis/',coverName,'.stl']);
+    opts = detectImportOptions('./srcPressureAnalysis/chest_sensors.txt');
+    pressureSensors = table2struct(readtable(['./srcPressureAnalysis/',coverName,'_sensors.txt'],opts),"ToScalar",true);
+    pressureSensorsNames = pressureSensors.Var1;
+    x_pressureSensors    = pressureSensors.Var2;
+    y_pressureSensors    = pressureSensors.Var3;
+    z_pressureSensors    = pressureSensors.Var4;
 
-% load a single point data
-testPoint.(testPointIDs{1}) = load([folderPath,testID{1},'/pressureSensorsData/',testPointIDs{1},'.mat']);
+    %% LOAD PRESSURE DATA
+    addpath(genpath('../'));            % Adding the main folder path
+    experiment = 'exp_21_03_22';        % Name of the experiment data folder
+    folderPath = ['../',experiment,'/data_Matlab/'];    % Path to the experiment data folder
+    testPoint.(testPointID{1}) = load([folderPath,testID{1},'/pressureSensorsData/',testPointID{1},'.mat']);  % test point loading
 
-% assign mean pressure values data
-meanPressValues = zeros(length(pressureSensorsNames),1);
-pressValues     = zeros(length(pressureSensorsNames),length(testPoint.(testPointIDs{1}).pressureSensors.time));
-for i = 1:length(meanPressValues)
-    meanPressValues(i) = testPoint.(testPointIDs{1}).pressureSensors.meanValues.(pressureSensorsNames{i});
-    pressValues(i,:)   = testPoint.(testPointIDs{1}).pressureSensors.values.(pressureSensorsNames{i});
-end
-
-fprintf([num2str(min(min(pressValues)),3),' [Pa] \x2264 \x394p \x2264 ',num2str(max(max(pressValues)),3),' [Pa] \n']);
-fprintf(['\x394T = ',num2str(testPoint.(testPointIDs{1}).pressureSensors.time(end),3),' [s] \n']);
-fprintf(['rt_foot \x394p = ',num2str(testPoint.(testPointIDs{1}).pressureSensors.meanValues.S1,3),' [Pa] \n']);
-fprintf(['lt_foot \x394p = ',num2str(testPoint.(testPointIDs{1}).pressureSensors.meanValues.S3,3),' [Pa] \n']);
-
-%% DATA PLOTTING
-figure()
-
-h = pdegplot(geom); hold on
-[X,Y,Z] = sphere;
-radius  = 3;
-
-cmap = colormap("jet");
-cmap = interp1(linspace(min(meanPressValues),max(meanPressValues),length(cmap)),cmap,meanPressValues);
-
-for i=1:length(x_pressureSensors)
-surf(X*radius + x_pressureSensors(i), Y*radius + y_pressureSensors(i), Z*radius + z_pressureSensors(i), ...
-     'FaceColor',cmap(i,:),'EdgeColor','none','FaceLighting','none')
-end
-axis([-100 100 -150 100 0 150])
-
-if contains(coverName,'lt_thigh_front') || contains(coverName,'lt_shin_front') || contains(coverName,'face_back')
-    view(180,270); light('Position',[1,1,-1]);   % lt_thigh_front, lt_shin_front
-else 
-    if contains(coverName,'lt_arm_front')
-        view(0,0); light('Position',[1,-1,1]);
-    else
-        view(0,90); light();                         % other cases
+    % assign pressure values data
+    meanPressValues = zeros(length(pressureSensorsNames),1);
+    pressValues     = zeros(length(pressureSensorsNames),length(testPoint.(testPointID{1}).pressureSensors.time));
+    for i = 1:length(meanPressValues)
+        meanPressValues(i) = testPoint.(testPointID{1}).pressureSensors.meanValues.(pressureSensorsNames{i});
+        pressValues(i,:)   = testPoint.(testPointID{1}).pressureSensors.values.(pressureSensorsNames{i});
     end
-end
-colorbar
-caxis([min(meanPressValues) max(meanPressValues)])
+    
+    localMinPress = min(min(pressValues));
+    localMaxPress = max(max(pressValues));
+    fprintf([coverName,' pressure range: ',num2str(localMinPress,3),' [Pa] \x2264 \x394p \x2264 ',num2str(localMaxPress,3),' [Pa] \n']);
 
-figure()
-for i = 1:length(meanPressValues)
-    plot(testPoint.(testPointIDs{1}).pressureSensors.time,pressValues(i,:),"Color",cmap(i,:)); hold on;
+    if localMinPress < globalMinPress
+        globalMinPress = localMinPress;
+    end
+    if localMaxPress > globalMaxPress
+        globalMaxPress = localMaxPress;
+    end
+
+    %% DATA PLOTTING INITIALIZATION
+    % initialize generic sphere coordinates
+    [X,Y,Z] = sphere;
+    radius  = 3;
+    
+    
+    %% PLOT PRESSURE MAP
+    fig = figure();
+    h = pdegplot(geom); hold on     % plot geometry
+    cmap = colormap("jet");         % initialize colormap
+    cmap = interp1(linspace(min(meanPressValues),max(meanPressValues),length(cmap)),cmap,meanPressValues);
+    for i=1:length(x_pressureSensors)
+        surf(X*radius + x_pressureSensors(i), Y*radius + y_pressureSensors(i), Z*radius + z_pressureSensors(i), ...
+            'FaceColor',cmap(i,:),'EdgeColor','none','FaceLighting','none');
+    end
+    if contains(coverName,'face_front')
+        axis([-100 100 -100 150 0 150])
+    else
+        axis([-100 100 -150 100 0 150])
+    end
+    colorbar
+    caxis([min(meanPressValues) max(meanPressValues)])
+    title(coverName,'Interpreter','none')
+    
+    if contains(coverName,'lt_thigh_front') || contains(coverName,'lt_shin_front') || contains(coverName,'face_back')
+        view(180,270); light('Position',[1,1,-1]);   % lt_thigh_front, lt_shin_front
+        else
+            if contains(coverName,'lt_arm_front')
+                view(0,0); light('Position',[1,-1,1]);
+            else
+                view(0,90); light();                         % other cases
+            end
+    end
+    
+    saveas(fig,['.\postProcess\',coverName,'-',testID{1},'-',testPointID{1},'.svg']);
+
+    %% PLOT TIME-VARYING PRESSURES
+    fig = figure();
+    for i = 1:length(meanPressValues)
+        plot(testPoint.(testPointID{1}).pressureSensors.time,pressValues(i,:),"Color",cmap(i,:)); hold on;
+    end
+    grid on;
+    ylabel('$\Delta p$ [Pa]','Interpreter','latex')
+    xlabel('$t$ [s]','Interpreter','latex')
+    title(coverName,'Interpreter','none')
+
+    saveas(fig,['.\postProcess\',coverName,'-',testID{1},'-',testPointID{1},'-plot.svg']);
 end
-grid on;
-ylabel('$\Delta p$ [Pa]','Interpreter','latex')
-xlabel('$t$ [s]','Interpreter','latex')
+
+%% DATA REPORTING
+fprintf(['rt_foot \x394p = ',num2str(testPoint.(testPointID{1}).pressureSensors.meanValues.S1,3),' [Pa] \n']);
+fprintf(['lt_foot \x394p = ',num2str(testPoint.(testPointID{1}).pressureSensors.meanValues.S3,3),' [Pa] \n\n']);
+fprintf(['Global pressure range: ',num2str(globalMinPress,3),' [Pa] \x2264 \x394p \x2264 ',num2str(globalMaxPress,3),' [Pa] \n']);
+fprintf(['Total acquisition time: \x394T = ',num2str(testPoint.(testPointID{1}).pressureSensors.time(end),3),' [s] \n']);
+
 
 %% Remove from path
 
