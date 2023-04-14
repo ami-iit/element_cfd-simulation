4510f3333481f1d2ca6a5d1a568d590057d3310c
Antonello Paolino <antonello.paolino@iit.it>
2023-04-14 11:08:43 +0200

    Upload more efficient post-process script

--- wind-tunnel/scripts/aerodynamicForcesPostProcess.m
+++ wind-tunnel/scripts/aerodynamicForcesPostProcess.m
@@ -9,7 +9,7 @@ experimentPath = ['../',experiment,'/data_Matlab/'];
 
 %% Load test variables
 
-testIDs = {'TID_0059'};   % testIDs to be loaded in the script
+testIDs = {'TID_0015'};   % testIDs to be loaded in the script
 
 for i = 1: length(testIDs)
     test.(testIDs{i}) = load([experimentPath,testIDs{i},'/aerodynamicForces.mat']);
@@ -24,14 +24,15 @@ yVariable = {'dragForceCoeff',  'liftForceCoeff',   'sideForceCoeff', ...
 yLabel = {'$C_D A$','$C_L A$','$C_S A$','$C_r A l$','$C_p A l$','$C_y A l$'};
 
 % assign the legend names for the plots
-displayNames = {'exp: flight60'};
+displayNames = {'exp: flight30, $\beta=30^\circ$','exp: flight50, $\beta=30^\circ$','exp: flight60, $\beta=30^\circ$'};
 
 % set the plot lines appeareance
-colors = {[0 0 0],[0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.9290, 0.6940, 0.1250], [0,0.5,0], [1,0,1]};
-lineStyles = {'-','-','-','-','-','-','-','-'};
+colors = {[0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0.9290, 0.6940, 0.1250], ...
+          [0, 0, 0], [0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.9290, 0.6940, 0.1250], [0,0.5,0], [1,0,1]};
+lineStyles = {'--','--',':',':','--','--','-','-'};
 
 % setting x axis limits
-xLimits = [20 70];
+xLimits = [0 90.1];
 
 
 %% assigning alpha or beta plotting
@@ -48,7 +49,7 @@ end
 
 %% CFD DATA 
 
-simulationNames = {'flight60_2'};
+simulationNames = {'hovering_2','hovering_2_ROM'};
 
 cfdPath = '../../cfd/wind-tunnel/results/';
 opts    = detectImportOptions([cfdPath,'flight30_2.txt']);
@@ -59,173 +60,37 @@ end
 
 
 %% PLOTS
+variable = {'CdA_ke','CdA_kw','ClA_ke','ClA_kw','CsA_ke','CsA_kw', ...
+            'CrAl_ke','CrAl_kw','CpAl_ke','CpAl_kw','CyAl_ke','CyAl_kw'};
 
-figure()
-% fig1 = openfig("fig1.fig");
+for varIndex = 1:length(variable)/2
+    
+    figure(varIndex)
 
-% plot CFD data
-for i = 1 : length(simulationNames)
-    if matches(xVariable,'betaMeas')
-        cfdPlotAngle = 'beta';
-    else
-        cfdPlotAngle = 'alpha';
+    % plot CFD data
+    for i = 1 : length(simulationNames)
+        if matches(xVariable,'betaMeas')
+            cfdPlotAngle = 'beta';
+        else
+            cfdPlotAngle = 'alpha';
+        end
+        plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).(variable{varIndex*2-1}),'Color',colors{i*2-1},'LineStyle',lineStyles{i*2-1}, ...
+             'linewidth',2,'Marker','o','DisplayName',['CFD: ',simulationNames{i}(1:8),', Realizable $k-\varepsilon$']); hold on;
+        plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).(variable{varIndex*2}),'Color',colors{i*2},'LineStyle',lineStyles{i*2}, ...
+             'linewidth',2,'Marker','o','DisplayName',['CFD: ',simulationNames{i}(1:8),', SST $k-\omega$']); hold on;
     end
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CdA_ke,'Color','b','LineStyle','--','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: Realizable $k-\varepsilon$'); hold on;
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CdA_kw,'Color','r','LineStyle',':','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: SST $k-\omega$'); hold on;
+
+    % plot experimental data
+    for i = 1 : length(testIDs)
+        plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{varIndex}), ...
+            'Color','k','LineStyle','-','linewidth',2,'DisplayName',displayNames{i});hold on;
+    end
+
+    grid on;
+    xlim(xLimits)
+    ylabel(yLabel{varIndex},'Interpreter','latex')
+    xlabel(xLabel,'Interpreter','latex')
+    legend('Interpreter','latex','Location','best')
+    legend show
+
 end
-
-% plot experimental data
-for i = 1 : length(testIDs)
-    plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{1}), ...
-         'Color',colors{i},'LineStyle',lineStyles{i},'linewidth',1.5,'DisplayName',displayNames{i});hold on;
-end
-
-grid on;
-% xlim([min(test.(testIDs{1}).state.(x))+OffSet ...
-%       max(test.(testIDs{1}).state.(x))+OffSet])
-xlim(xLimits)
-ylabel(yLabel{1},'Interpreter','latex')
-xlabel(xLabel,'Interpreter','latex')
-legend('Interpreter','latex','Location','best')
-legend show
-
-
-figure()
-% fig2 = openfig("fig2.fig");
-
-% plot CFD data
-for i = 1 : length(simulationNames)
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).ClA_ke,'Color','b','LineStyle','--','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: Realizable $k-\varepsilon$'); hold on;
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).ClA_kw,'Color','r','LineStyle',':','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: SST $k-\omega$'); hold on;
-end
-
-% plot experimental data
-for i = 1 : length(testIDs)
-    plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{2}), ...
-         'Color',colors{i},'LineStyle',lineStyles{i},'linewidth',1.5, 'DisplayName', displayNames{i}); hold on;
-end
-
-grid on;
-% xlim([min(test.(testIDs{1}).state.(x))+OffSet ...
-%       max(test.(testIDs{1}).state.(x))+OffSet])
-xlim(xLimits)
-ylabel(yLabel{2},'Interpreter','latex')
-xlabel(xLabel,'Interpreter','latex')
-legend('Interpreter','latex','Location','best')
-legend show
-
-
-figure() 
-% fig3 = openfig("fig3.fig"); 
-
-% plot CFD data
-for i = 1 : length(simulationNames)
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CsA_ke,'Color','b','LineStyle','--','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: Realizable $k-\varepsilon$'); hold on;
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CsA_kw,'Color','r','LineStyle',':','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: SST $k-\omega$'); hold on;
-end
-
-% plot experimental data
-for i = 1 : length(testIDs)
-
-      plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{3}), ...
-          'Color',colors{i},'LineStyle',lineStyles{i},'linewidth',1.5, 'DisplayName', displayNames{i}); hold on;
-end
-
-grid on;
-% xlim([min(test.(testIDs{1}).state.(x))+OffSet ...
-%       max(test.(testIDs{1}).state.(x))+OffSet])
-xlim(xLimits)
-ylabel(yLabel{3},'Interpreter','latex')
-xlabel(xLabel,'Interpreter','latex')
-legend('Interpreter','latex','Location','best')
-legend show
-
-
-figure()
-% fig4 = openfig("fig4.fig");
-
-% plot CFD data
-for i = 1 : length(simulationNames)
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CrAl_ke,'Color','b','LineStyle','--','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: Realizable $k-\varepsilon$'); hold on;
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CrAl_kw,'Color','r','LineStyle',':','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: SST $k-\omega$'); hold on;
-end
-
-% plot experimental data
-for i = 1 : length(testIDs)
-
-    plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{4}), ...
-         'Color',colors{i},'LineStyle',lineStyles{i},'linewidth',1.5, 'DisplayName', displayNames{i}); hold on;
-end
-
-grid on;
-% xlim([min(test.(testIDs{1}).state.(x))+OffSet ...
-%       max(test.(testIDs{1}).state.(x))+OffSet])
-xlim(xLimits)
-ylabel(yLabel{4},'Interpreter','latex')
-xlabel(xLabel,'Interpreter','latex')
-legend('Interpreter','latex','Location','best')
-legend show
-
-
-figure()
-% fig5 = openfig("fig5.fig"); 
-
-% plot CFD data
-for i = 1 : length(simulationNames)
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CpAl_ke,'Color','b','LineStyle','--','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: Realizable $k-\varepsilon$'); hold on;
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CpAl_kw,'Color','r','LineStyle',':','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: SST $k-\omega$'); hold on;
-end
-
-% plot experimental data
-for i = 1 : length(testIDs)
-
-    plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{5}), ...
-         'Color',colors{i},'LineStyle',lineStyles{i},'linewidth',1.5, 'DisplayName', displayNames{i}); hold on;
-end
-
-grid on;
-% xlim([min(test.(testIDs{1}).state.(x))+OffSet ...
-%       max(test.(testIDs{1}).state.(x))+OffSet])
-xlim(xLimits)
-ylabel(yLabel{5},'Interpreter','latex')
-xlabel(xLabel,'Interpreter','latex')
-legend('Interpreter','latex','Location','best')
-legend show
-
-
-figure()
-% fig6 = openfig("fig6.fig"); 
-
-% plot CFD data
-for i = 1 : length(simulationNames)
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CyAl_ke,'Color','b','LineStyle','--','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: Realizable $k-\varepsilon$'); hold on;
-    plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).CyAl_kw,'Color','r','LineStyle',':','linewidth',1.5,...
-         'Marker','o','DisplayName','CFD: SST $k-\omega$'); hold on;
-end
-
-% plot experimental data
-for i = 1 : length(testIDs)
-
-    plot(test.(testIDs{i}).state.(xVariable) + OffSet, test.(testIDs{i}).windAxesAero.(yVariable{6}), ...
-         'Color',colors{i},'LineStyle',lineStyles{i},'linewidth',1.5, 'DisplayName', displayNames{i}); hold on;
-end
-
-grid on;
-% xlim([min(test.(testIDs{1}).state.(x))+OffSet ...
-%       max(test.(testIDs{1}).state.(x))+OffSet])
-xlim(xLimits)
-ylabel(yLabel{6},'Interpreter','latex')
-xlabel(xLabel,'Interpreter','latex')
-legend('Interpreter','latex','Location','best')
-legend show
