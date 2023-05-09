ca4ba7e8f9d31a22c9b9e1ce872282c769b20d68
Antonello Paolino <antonello.paolino@iit.it>
2023-05-09 10:49:13 +0200

    Upload all the results from mesh study

--- wind-tunnel/scripts/aerodynamicForcesPostProcess.m
+++ wind-tunnel/scripts/aerodynamicForcesPostProcess.m
@@ -9,7 +9,7 @@ experimentPath = ['../',experiment,'/data_Matlab/'];
 
 %% Load test variables
 
-testIDs = {'TID_0015'};   % testIDs to be loaded in the script
+testIDs = {'TID_2017'};   % testIDs to be loaded in the script
 
 for i = 1: length(testIDs)
     test.(testIDs{i}) = load([experimentPath,testIDs{i},'/aerodynamicForces.mat']);
@@ -24,15 +24,16 @@ yVariable = {'dragForceCoeff',  'liftForceCoeff',   'sideForceCoeff', ...
 yLabel = {'$C_D A$','$C_L A$','$C_S A$','$C_r A l$','$C_p A l$','$C_y A l$'};
 
 % assign the legend names for the plots
-displayNames = {'exp: flight30, $\beta=30^\circ$','exp: flight50, $\beta=30^\circ$','exp: flight60, $\beta=30^\circ$'};
+displayNames = {'exp: flight30'};
 
 % set the plot lines appeareance
-colors = {[0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0.9290, 0.6940, 0.1250], ...
-          [0, 0, 0], [0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.9290, 0.6940, 0.1250], [0,0.5,0], [1,0,1]};
-lineStyles = {'--','--',':',':','--','--','-','-'};
+colors = {[0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], ...
+          [0, 0.4470, 0.7410], [0.8500, 0.3250, 0.0980], [0, 0.22, 0.37], [0.42, 0.16, 0.0980],...
+          [0.9290, 0.6940, 0.1250], [0, 1, 0]};
+lineStyles = {'-','-','--','--','-.','-.',':',':',':',':'};
 
 % setting x axis limits
-xLimits = [0 90.1];
+xLimits = [20 70];
 
 
 %% assigning alpha or beta plotting
@@ -49,10 +50,11 @@ end
 
 %% CFD DATA 
 
-simulationNames = {'hovering_2','hovering_2_ROM'};
+simulationNames = {'flight30_2_953C','flight30_2_202C','flight30_2_050C','flight30_2_024C','flight30_2_011C'};
+cfdPlotNames    = {'flight30-953C','flight30-202C','flight30-050C','flight30-024C','flight30-011C'};
 
 cfdPath = '../../cfd/wind-tunnel/results/';
-opts    = detectImportOptions([cfdPath,'flight30_2.txt']);
+opts    = detectImportOptions([cfdPath,'flight30_2_953C.txt']);
 
 for i = 1:length(simulationNames)
     cfdData.(simulationNames{i}) = table2struct(readtable([cfdPath,simulationNames{i},'.txt']),"ToScalar",true);
@@ -74,10 +76,10 @@ for varIndex = 1:length(variable)/2
         else
             cfdPlotAngle = 'alpha';
         end
-        plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).(variable{varIndex*2-1}),'Color',colors{i*2-1},'LineStyle',lineStyles{i*2-1}, ...
-             'linewidth',2,'Marker','o','DisplayName',['CFD: ',simulationNames{i}(1:8),', Realizable $k-\varepsilon$']); hold on;
+%         plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).(variable{varIndex*2-1}),'Color',colors{i*2-1},'LineStyle',lineStyles{i*2-1}, ...
+%              'linewidth',2,'Marker','o','DisplayName',['CFD: ',cfdPlotNames{i},', Realizable $k-\varepsilon$']); hold on;
         plot(cfdData.(simulationNames{i}).(cfdPlotAngle),cfdData.(simulationNames{i}).(variable{varIndex*2}),'Color',colors{i*2},'LineStyle',lineStyles{i*2}, ...
-             'linewidth',2,'Marker','o','DisplayName',['CFD: ',simulationNames{i}(1:8),', SST $k-\omega$']); hold on;
+             'linewidth',2,'Marker','o','DisplayName',['CFD: ',cfdPlotNames{i},', SST $k-\omega$']); hold on;
     end
 
     % plot experimental data
