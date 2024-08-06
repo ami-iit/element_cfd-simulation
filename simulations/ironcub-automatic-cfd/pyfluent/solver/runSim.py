acfafbb4193fda6d5c894a976e09756527feadcc
Antonello Paolino <antonello.paolino@iit.it>
2024-08-06 13:38:39 +0200

    Update runSim for latest simulation set

--- simulations/ironcub-automatic-cfd/pyfluent/solver/runSim.py
+++ simulations/ironcub-automatic-cfd/pyfluent/solver/runSim.py
@@ -15,7 +15,6 @@ https://github.com/ansys/pyfluent/tree/v0.19.2/examples/00-fluent
 
 import ansys.fluent.core as pyfluent
 from datetime import datetime
-
 import numpy as np
 import pathlib
 import os
@@ -34,14 +33,13 @@ if robotName == "ironcub-mk1":
 elif robotName == "ironcub-mk3":
     import src.mk3 as robot
 
-
 ###############################################################################
 # Set the Fluent configuration parameters
 # ~~~~~~~~~~~~~
 # Set the parameters for the Fluent session
 
-core_number = 12        # number of cores (only pre-post if use_gpu=True)
-use_gpu = True          # use GPU native solver
+core_number = 12 # number of cores (only pre-post if use_gpu=True)
+use_gpu = True # use GPU native solver
 iteration_number = 10 # number of iterations to run in the solver
 
 # Set the MPI option for the WS
@@ -53,19 +51,14 @@ mpi_option = "-mpi=openmpi" if os.name == "posix" else ""
 # Define the paths for the directories and the files used in the process.
 
 # Define dir paths
-
 rootPath = pathlib.Path(__file__).parents[0]
-
+parentPath = pathlib.Path(__file__).parents[1]
 dataDirPath = rootPath / "data" / robotName[-3:] # data directory path
 logDirPath = rootPath / "log" # log directory path
 srcDirPath = rootPath / "src" # source directory path
-
-parentPath = pathlib.Path(__file__).parents[1]
-
 caseDirPath = parentPath / "meshing" / "case" / robotName[-3:] # Fluent case directory path
 
 # Define file paths
-
 pitchAnglesFilePath = srcDirPath / "pitchAngles.csv" # pitch angles file path
 yawAnglesStartFilePath = srcDirPath / "yawAnglesStart.csv" # yaw angles start file path
 yawAnglesFilePath = srcDirPath / "yawAngles.csv" # yaw angles file path
@@ -96,11 +89,7 @@ contoursPath = dataDirPath / dataDirectories[1]
 databasePath = dataDirPath / dataDirectories[2]
 databaseExtendedPath = dataDirPath / dataDirectories[3]
 
-###############################################################################
-# Create the output parameters file
-# ~~~~~~~~~~~~~
 # Create the output parameters file if not existing.
-
 if not outputParamFilePath.exists():
     with open(str(outputParamFilePath), 'w') as outputParamCSV:
         outputParameterHeader = "config,pitchAngle,yawAngle,ironcub-cd,ironcub-cl,ironcub-cs"
@@ -122,7 +111,6 @@ yawAngleList = getAnglesList(yawAnglesFilePath)
 outParamList = getOutputParameterList(outputParamFilePath)
 jointConfigNames = getJointConfigNames(jointConfigFilePath)
 
-
 ###############################################################################
 # Start the automatic process
 # ~~~~~~~~~~~~~
@@ -132,7 +120,6 @@ jointConfigNames = getJointConfigNames(jointConfigFilePath)
 for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
 
     # Define list of yaw angles (different for first config to restart a crushed process)
-
     yawAngleCycleList = yawAngleStartList if jointConfigIndex == 0 else yawAngleList
 
     ###############################################################################
@@ -146,13 +133,12 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
     solver = pyfluent.launch_fluent(
         mode="solver",                      # "meshing", "pure-meshing" or "solver"
         precision="double",                 # single or double precision
-        product_version="23.2.0",           # Fluent version
-        version="3d",                       # 2d or 3d Fluent version
+        product_version="24.1.0",           # Fluent version
+        dimension=3,                       # 2d or 3d Fluent version
         processor_count=core_number,        # number of cores (only pre-post if use_gpu=True)
         gpu=use_gpu,                        # use GPU native solver
         start_transcript=False,             # start transcript file
         cwd=str(logDirPath),                # working directory
-        show_gui=False,                     # show GUI or not
         additional_arguments=mpi_option,    # additional arguments (used for MPI option)
     )
 
@@ -165,85 +151,58 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
 
         # Singular configuration check:
         # |yawAngle| == 90 => wind direction not changing with pitch angle
-
         pitchAngleCycleList = pitchAngleList if ( abs(abs(yawAngle) - 90) > 1e-4 ) else [0.0]
-
 
         ###############################################################################
         # Start the cycle on pitch angles
         # ~~~~~~~~~~~~~
         # Start a cycle on all the pitch angles for each joint configuration to run.
-
         for pitchAngle in pitchAngleCycleList:
 
-            ###############################################################################
-            # Read the case file
-            # ~~~~~~~~~~~~~
             # Read the case file for the current joint configuration
-
             caseFileName = jointConfigName + ".cas.h5"
             caseFilePath = caseDirPath / caseFileName
             solver.file.read_case(file_name=str(caseFilePath))
 
-
-            ###############################################################################
-            # Modify the mesh
-            # ~~~~~~~~~~~~~
             # Rotate the mesh according to pitch and yaw angles and set the correct
             # Boundary Conditions.
-
             solver.mesh.rotate(
                 angle=np.deg2rad(pitchAngle),
                 origin=[0, 0, 0],
                 axis_components=[-1, 0, 0],
             )
-
             solver.mesh.rotate(
                 angle=np.deg2rad(yawAngle),
                 origin=[0, 0, 0],
                 axis_components=[0, 1, 0],
             )
-
-            solver.tui.mesh.modify_zones.sep_face_zone_mark("inlet", "region_in", "yes")
-            solver.tui.mesh.modify_zones.zone_type("inlet", "pressure-outlet")
-
+            
+            # Boundary Conditions (TODO: fix to be removed in the future)
+            inlet = solver.setup.boundary_conditions.velocity_inlet["inlet"]
+            inlet.turbulence.turbulent_intensity = 0.001
+            
+            solver.mesh.modify_zones.sep_face_zone_mark(face_zone_name="inlet",register_name="region_in")
+            solver.mesh.modify_zones.zone_type(zone_names = ["inlet"], new_type = "pressure-outlet")
 
             ###############################################################################
-            # Initialize flow field
+            # Initialize and solve the flow field
             # ~~~~~~~~~~~~~~~~~~~~~
-            # Initialize the flow field using hybrid initialization.
+            # Initialize using hybrid initialization and solve the flow field.
 
             solver.solution.initialization.hybrid_initialize()
-
-
-            ###############################################################################
-            # Solve the flow field
-            # ~~~~~~~~~~~~~~~~~~~~~~~~
-            # Solve the flow field for the current joint configuration and angles.
-
             solver.solution.run_calculation.iterate(iter_count=iteration_number)
-
 
             ###############################################################################
             # Plot and save residuals
             # ~~~~~~~~~~~~~~~~~~~~~~~~
             # Plot and save the residuals for the current joint configuration and angles.
 
-            solver.tui.plot.residuals("y y y y y y")
-            solver.tui.display.set.picture.driver.jpeg()
             solver.results.graphics.picture.use_window_resolution = False
             solver.results.graphics.picture.x_resolution = 1920
             solver.results.graphics.picture.y_resolution = 1440
-
-            solver.tui.display.save_picture(
-                str( residualsPath / f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}" )
-            )
-
-            # The designed command was not working:
-            # solver.results.graphics.picture.save_picture(
-            #     str(residualsPath / f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}")
-            #     )
-
+            solver.results.graphics.picture.save_picture(
+                file_name = str( residualsPath / f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}")
+                )
 
             ###############################################################################
             # Plot and save contours
@@ -253,7 +212,7 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
             solver.results.graphics.contour["velocity-contour"] = {}
             solver.results.graphics.contour["velocity-contour"] = {
                 "field": "velocity-magnitude",
-                "surfaces_list": ["yz_plane"],
+                "surfaces_list": ["yz-plane"],
                 "node_values": True,
                 "range_option": {
                     "option": "auto-range-on",
@@ -262,38 +221,21 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
             }
             solver.results.graphics.contour.display(object_name="velocity-contour")
             solver.results.graphics.views.restore_view(view_name="right")
-            solver.results.graphics.picture.use_window_resolution = False
-            solver.results.graphics.picture.x_resolution = 1920
-            solver.results.graphics.picture.y_resolution = 1440
-
-            solver.tui.display.save_picture(
-                str( contoursPath / f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}" )
-            )
-
-            # The designed command was not working:
-            # solver.results.graphics.picture.save_picture(
-            #     str( contoursPath / f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}" )
-            # )
-
+            solver.results.graphics.picture.save_picture(
+                file_name = str( contoursPath / f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}")
+                )
 
             ###############################################################################
             # Compute and write output parameters
             # ~~~~~~~~~~~~~~~~~~~~~~~~
             # Compute and write the output parameters
             
-            outParamValList = solver.solution.report_definitions.compute(
-                report_defs=outParamList
-            )
-
+            outParamValList = solver.solution.report_definitions.compute(report_defs=outParamList)
             with open(str(outputParamFilePath), "a") as outputParamCSV:
-
                 outputParameterString = f"{jointConfigName},{pitchAngle},{yawAngle}"
-
                 for outParamIndex, _ in enumerate(outParamList):
-
                     outParamVal = outParamValList[outParamIndex][outParamList[outParamIndex]][0]
                     outputParameterString = outputParameterString + f",{outParamVal}"
-
                 outputParamCSV.writelines(outputParameterString + "\n")
 
             ###############################################################################
@@ -302,7 +244,7 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
             # Export the pressure and shear stress data on each single surface.
 
             cd_report = solver.solution.report_definitions.drag["ironcub-cd"]
-            surfaceList = cd_report.thread_names.allowed_values()
+            surfaceList = cd_report.zones.allowed_values()
             
             # Export database files for each merge surface
             for reportSurface in robot.ironcubSurfacesList:
@@ -328,13 +270,13 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
                     databaseFileName = f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}-{reportSurface}.dtbs"
                     databaseFilePath = str( databasePath / databaseFileName )
                     solver.file.export.ascii(
-                        name=databaseFilePath,
+                        file_name=databaseFilePath,
                         surface_name_list=reportSurfaceList,
                         delimiter="space",
                         cell_func_domain=["pressure", "x-wall-shear", "y-wall-shear", "z-wall-shear"],
                         location="node",
                     )
-        
+
             # Export database files for each single surface
             for reportSurface in robot.ironcubSurfacesList:
                 reportSurfaceList = [reportSurface]
@@ -345,7 +287,7 @@ for jointConfigIndex, jointConfigName in enumerate(jointConfigNames):
                 databaseFileName = f"{jointConfigName}-{int(pitchAngle)}-{int(yawAngle)}-{reportSurface}.dtbs"
                 databaseFilePath = str( databaseExtendedPath / databaseFileName )
                 solver.file.export.ascii(
-                    name=databaseFilePath,
+                    file_name=databaseFilePath,
                     surface_name_list=reportSurfaceList,
                     delimiter="space",
                     cell_func_domain=["pressure", "x-wall-shear", "y-wall-shear", "z-wall-shear"],
