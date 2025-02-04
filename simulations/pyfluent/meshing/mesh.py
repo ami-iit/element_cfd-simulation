6ccd80217b3264e97fd943e6a01a04a51eacb791
Antonello Paolino <antonello.paolino@iit.it>
2025-02-03 19:00:26 -0800

    Restructure meshing output

--- simulations/pyfluent/meshing/mesh.py
+++ simulations/pyfluent/meshing/mesh.py
@@ -49,14 +49,11 @@ def main():
 
     # Define output directories
     msh_dir = root_dir / "mesh" / ROBOT_NAME[-3:] / "msh"
-    dtbs_dir = root_dir / "mesh" / ROBOT_NAME[-3:] / "dtbs"
     cas_dir = root_dir / "case" / ROBOT_NAME[-3:]
 
     # Create output directories if they do not exist
     msh_dir.mkdir(parents=True, exist_ok=True)
     print_log("info", f"{msh_dir.stem} path: {msh_dir}", log_file)
-    dtbs_dir.mkdir(parents=True, exist_ok=True)
-    print_log("info", f"{dtbs_dir.stem} path: {dtbs_dir}", log_file)
     cas_dir.mkdir(parents=True, exist_ok=True)
     print_log("info", f"{cas_dir.stem} path: {cas_dir}", log_file)
 
@@ -368,27 +365,6 @@ def main():
             cas_file_name = config_name + ".cas.h5"
             cas_file_path = cas_dir / cas_file_name
             solver.file.write(file_name=str(cas_file_path), file_type="case")
-
-            # Export mesh dtbs files
-            cd_report = solver.solution.report_definitions.drag["ironcub-cd"]
-            surface_list = cd_report.zones.allowed_values()
-
-            # Export database files for each single surface
-            for rep_surf in robot.surfaces_list:
-                rep_surf_list = [rep_surf]
-                rep_surf_pref = rep_surf + ":"
-                for surface in surface_list:
-                    if rep_surf_pref in surface:
-                        rep_surf_list.extend([surface])
-                dtbs_file_name = f"mesh-{config_name}-0-0-{rep_surf}.dtbs"
-                dtbs_file_path = str(dtbs_dir / dtbs_file_name)
-                solver.file.export.ascii(
-                    file_name=dtbs_file_path,
-                    surface_name_list=rep_surf_list,
-                    delimiter="space",
-                    cell_func_domain=["x-face-area", "y-face-area", "z-face-area"],
-                    location="cell-center",
-                )
 
             # Close Fluent and clean up debug files
             solver.exit()
